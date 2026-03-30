a26f8be6a27c5b9d7a93c413c6b22b8f250a8e25
hwchung24 <54504072+hwchung24@users.noreply.github.com>
2026-03-30 18:29:52 +0900

    Update article className to use snap-center for horizontal scroll snapping.

---
+++ src/components/StudentLifestyleSection.tsx
@@ -0,0 +1,3 @@
+<article className="relative h-[min(52vh,420px)] w-[min(88vw,360px)] shrink-0 snap-center overflow-hidden rounded-2xl sm:h-[min(50vh,440px)] sm:w-[min(72vw,380px)] lg:w-[380px]">
+  {/* Article content here */}
+</article>
