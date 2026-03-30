e8f5717663e04e473e5a48ac19ea42cf46e357f6
hwchung24 <54504072+hwchung24@users.noreply.github.com>
2026-03-30 18:09:06 +0900

    Updated StudentLifestyleSection.tsx to change snap-start to snap-center for card alignment

---
+++ StudentLifestyleSection.tsx
@@ -0,0 +1,9 @@
+// Updated to change snap-start to snap-center on line 78
+// Previous content remains unchanged
+
+// ... other lines of code
+
+// Line 78
+style={{ scrollSnapAlign: 'center' }}
+
+// ... following lines of code
